ba8ace796d062bfb938797a8db6d04f14182c972
jizong <jizong.peng.1@etsmtl.net>
2020-06-29 11:56:42 -0400

    adding sup_contrast

--- script.py
+++ script.py
@@ -24,7 +24,7 @@ baseline = [
     f"python main_ce.py --batch_size {batch_size}  --learning_rate 0.8  --epochs={epoch} --cosine --save_dir=save/sup_ce > save/sup_ce.txt"
 ]
 
-proposed = [
+proposed1 = [
     # simclr
     f"python main_supcon.py --batch_size {batch_size}   --learning_rate 0.5  --temp 0.5  --epochs={epoch} --cosine --method SimCLR --cluster_regweigt=0.0001 --train_cluster --save_dir=save/simclr/cluster_0.0001 &&"
     f"python main_linear.py --batch_size {batch_size}   --learning_rate 1  --epochs={test_epoch}  --ckpt save/simclr/cluster_0.0001/last.pth > save/simclr/cluster_0.0001/result.txt",
@@ -38,11 +38,26 @@ proposed = [
     f"python main_supcon.py --batch_size {batch_size}   --learning_rate 0.5  --temp 0.5  --epochs={epoch} --cosine --method SimCLR --cluster_regweigt=0.1 --train_cluster --save_dir=save/simclr/cluster_0.1 &&"
     f"python main_linear.py --batch_size {batch_size}   --learning_rate 1  --epochs={test_epoch}  --ckpt save/simclr/cluster_0.1/last.pth > save/simclr/cluster_0.1/result.txt",
 ]
+proposed2 = [
+    # supcontrast
+    f"python main_supcon.py --batch_size {batch_size}   --learning_rate 0.5  --temp 0.5  --epochs={epoch} --cosine  --cluster_regweigt=0.0001 --train_cluster --save_dir=save/supcontrast/cluster_0.0001 &&"
+    f"python main_linear.py --batch_size {batch_size}   --learning_rate 1  --epochs={test_epoch}  --ckpt save/supcontrast/cluster_0.0001/last.pth > save/supcontrast/cluster_0.0001/result.txt",
 
-submitter = JobSubmiter(project_path="./", time=20, mem=32, gres="gpu:4", on_local=True,cpus_per_task=24)
+    f"python main_supcon.py --batch_size {batch_size}   --learning_rate 0.5  --temp 0.5  --epochs={epoch} --cosine  --cluster_regweigt=0.001 --train_cluster --save_dir=save/supcontrast/cluster_0.001 &&"
+    f"python main_linear.py --batch_size {batch_size}   --learning_rate 1  --epochs={test_epoch}  --ckpt save/supcontrast/cluster_0.001/last.pth > save/supcontrast/cluster_0.001/result.txt",
+
+    f"python main_supcon.py --batch_size {batch_size}   --learning_rate 0.5  --temp 0.5  --epochs={epoch} --cosine  --cluster_regweigt=0.01 --train_cluster --save_dir=save/supcontrast/cluster_0.01 &&"
+    f"python main_linear.py --batch_size {batch_size}   --learning_rate 1  --epochs={test_epoch}  --ckpt save/supcontrast/cluster_0.01/last.pth > save/supcontrast/cluster_0.01/result.txt",
+
+    f"python main_supcon.py --batch_size {batch_size}   --learning_rate 0.5  --temp 0.5  --epochs={epoch} --cosine  --cluster_regweigt=0.1 --train_cluster --save_dir=save/supcontrast/cluster_0.1 &&"
+    f"python main_linear.py --batch_size {batch_size}   --learning_rate 1  --epochs={test_epoch}  --ckpt save/supcontrast/cluster_0.1/last.pth > save/supcontrast/cluster_0.1/result.txt",
+    
+]
+
+submitter = JobSubmiter(project_path="./", time=20, mem=32, gres="gpu:4", on_local=False,cpus_per_task=24)
 submitter.prepare_env(
     ["source ./venv/bin/activate", "export OMP_NUM_THREADS=1 ", ]
 )
-for cmd in [*baseline, *proposed]:
+for cmd in [*baseline, *proposed1, *proposed2]:
     submitter.account = choice(account)
     submitter.run(cmd)
